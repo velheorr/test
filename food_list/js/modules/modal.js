eb1edf4822afa936fcf290dcee997ff914cfb5d4
Velheor <velheorr@gmail.com>
2021-10-20 16:52:54 +0500

    72 (module exports)

---
+++ food_list/js/modules/modal.js
@@ -0,0 +1,44 @@
+function modal() {
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal');
+
+    function openModal() {
+        modal.classList.toggle('show');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId)
+    }
+
+    modalTrigger.forEach(item =>{
+        item.addEventListener('click', openModal)
+    });
+
+    function closeModal(){
+        modal.classList.toggle('show');
+        document.body.style.overflow = ''
+    }
+
+    modal.addEventListener('click', (e)=>{
+        if(e.target === modal || e.target.getAttribute('data-close') == ''){
+            closeModal()
+        }
+    });
+
+    document.addEventListener('keydown', e =>{
+        if(e.code === 'Escape' && modal.classList.contains('show')){
+            closeModal()
+        }
+    });
+
+    const modalTimerId = setTimeout(openModal, 50000);
+
+    function showModalByScroll(){
+        if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight){
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll)
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll)
+}
+
+module.exports = modal;
